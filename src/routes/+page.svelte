66ccf9ed7ac9423adc9e3ce0b91be413cca7f4e2
gnituy18 <you@hsuyuting.com>
2023-02-20 23:37:50 +0800

    Bootstrap svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<header class="bg-white p-8">
+	<h1 class="text-3xl font-bold text-center">Social App</h1>
+</header>
+
+<main class="bg-white p-8">
+	<h2 class="text-2xl font-bold mb-8 text-center">Big Idea</h2>
+	<p class="text-lg text-center mb-8">
+		Connect with countless users and have rich text conversations with our innovative chat system.
+	</p>
+	<div class="text-center">
+		<a href="#" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-600">
+			Start Engaging
+		</a>
+	</div>
+
+	<section class="my-8">
+		<h2 class="text-2xl font-bold mb-8 text-center">Countless Users to Chat With</h2>
+		<p class="text-lg text-center mb-8">
+			With our app, you can connect with an endless amount of users and never run out of people to
+			talk to.
+		</p>
+	</section>
+
+	<section class="my-8">
+		<h2 class="text-2xl font-bold mb-8 text-center">Rich Text Chat System</h2>
+		<p class="text-lg text-center mb-8">
+			Our chat system allows for rich text formatting, including the ability to bold, italicize, and
+			underline text.
+		</p>
+	</section>
+
+	<section class="my-8">
+		<h2 class="text-2xl font-bold mb-8 text-center">Pricing</h2>
+		<div class="flex flex-col md:flex-row justify-around">
+			<div class="w-full md:w-1/3 mb-8 md:mb-0">
+				<h3 class="text-lg font-bold mb-4">Free One Month Trial</h3>
+				<p class="text-base">
+					Try our app for free for one month and see for yourself why it's the best chat app on the
+					market.
+				</p>
+			</div>
+			<div class="w-full md:w-1/3 mb-8 md:mb-0">
+				<h3 class="text-lg font-bold mb-4">$100 a Month</h3>
+				<p class="text-base">
+					For just $100 a month, enjoy all the features of our app without any limitations.
+				</p>
+			</div>
+			<div class="w-full md:w-1/3">
+				<h3 class="text-lg font-bold mb-4">$1000 a Year</h3>
+				<p class="text-base">
+					Pay just $1000 per year and enjoy all the features of our app without any limitations for
+					a full 12 months.
+				</p>
+			</div>
+		</div>
+	</section>
+</main>
+
+<footer class="bg-white p-8">
+	<p class="text-center text-gray-600">&copy; 2023 Social App</p>
+</footer>
